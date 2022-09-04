6cbb70af3845d259cff386325aa1c0529fd8948d
Zohaib Ahmed <zohaib4986@gmail.com>
2022-09-04 11:18:07 +0500

    asd

--- TESTING/For_Providers/terraform.tf
+++ TESTING/For_Providers/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     github = {
-      source  = "integrations/github"
-      version = "~> 4.0"
+      source = "integrations/github"
+      version = "4.31.0"
     }
   }
 }

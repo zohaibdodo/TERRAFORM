9b477e87394cee0979510442fba52d8cdc6212fb
Zohaib Ahmed <zohaib4986@gmail.com>
2022-09-16 10:03:24 +0500

    x

---
+++ TESTING/Multipleoutputinafile.tf
@@ -0,0 +1,11 @@
+output "A_output" {
+    value = "This is first output"
+}
+
+output "B_Output" {
+    value = "This is second output"
+}
+
+output "C_Output" {
+    value = "This is third output"
+}
---
+++ TESTING/functions/Onefunction.tf
@@ -0,0 +1,12 @@
+output printfirst {
+    
+}
+output helloworldupper {
+}
+   value = " $ { join ( " --- > " , var.users ) } "
+}
+   value = " $ { upper ( var.users [ 0 ] ) } "
+output helloworldlower {
+    value = " $ { lower ( var.users [ 1 ] ) } "
+output helloworldtitle {
+    value = " $ { title ( var.users [ 1 ] ) } "
